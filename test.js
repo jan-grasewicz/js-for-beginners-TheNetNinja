0b16cdf428bb0e46396bbc810036a54641a23c9f
Jan Grasewicz <j.grasewicz@gmail.com>
2018-12-22 19:14:23 +0100

    #20 function

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
-var links = document.getElementsByTagName("a");
-
-for(i = 0; i < links.length; i++){
-    links[i].className = "link-"+ i;
+function getAverage (a,b) {
+    var average = (a+b)/2;
+    console.log(average);
+    return average;
 }
+
+var myResult = getAverage(7,8);
+console.log("the average is "+myResult);
