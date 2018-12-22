e913ebcdf3b174863b4af0d87daaa4e898942122
Jan Grasewicz <j.grasewicz@gmail.com>
2018-12-22 20:10:51 +0100

    #25 slice, split

--- test.js
+++ test.js
@@ -1,11 +1,5 @@
-/*var myString = "I'm a \"fun\" ninja string"; 
-
-if (myString.indexOf("ninja")===-1){
-    console.log("the world ninja is not in the string");
-}else{
-    console.log("the word ninja starst at position " +
-    myString.indexOf("ninja"))
-}*/
-var string1 = "abc";
-var string2 = "ABC";
-console.log(string1.toLowerCase()===string2.toLowerCase());
+var str="Random sentence.";
+console.log(str.slice(2, 9));
+var tags="aa, bb, cc, dd, ee, ff";
+var tagsArray= tags.split(",");
+console.log(tagsArray)
