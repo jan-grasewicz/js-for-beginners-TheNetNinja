1ae6616bc8c7e63e815d9a24939274b113b9dde1
Jan Grasewicz <j.grasewicz@gmail.com>
2018-12-22 16:51:10 +0100

    basic commands

---
+++ test.js
@@ -0,0 +1,9 @@
+alert("Hello World");
+console.log("Hello");
+document.write("World");
+
+var myVar=10;
+myVar=myVar+5; //=== myVar+=5
+
+booleans(""); //false like 0, -0
+booleans("hello"); //true like all numbers exept 0
