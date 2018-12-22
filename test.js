f14adebc258f8a9b612ab5ae9f21ae581bd9c5a3
Jan Grasewicz <j.grasewicz@gmail.com>
2018-12-22 18:08:02 +0100

    while / for loop

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
-var myAge = 49;
-
-if (myAge > 30){
-    document.write("you are over 30");
-} else if (myAge > 20){
-    document.write("you are over 20");
-} else if (myAge > 10){
-    document.write("you are over 10");
-} else {
-    document.write("you are not over 10");
+var age=0;
+while (age<10){
+    console.log("your age is less than 10");
+    age++;
 }
+document.write("you are now over 10");
+
+// ==
+
+for (age=0; age<10; age++){
+    console.log("your age is less than 10");
+}
+document.write("you are now over 10");
