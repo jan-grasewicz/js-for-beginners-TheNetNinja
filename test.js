cd06f1c318dc764715da1e19f03ff0a3feb2496c
Jan Grasewicz <j.grasewicz@gmail.com>
2018-12-22 17:00:37 +0100

    if else

--- test.js
+++ test.js
@@ -1,9 +1,7 @@
-alert("Hello World");
-console.log("Hello");
-document.write("World");
+var myNum =10;
 
-var myVar=10;
-myVar=myVar+5; //=== myVar+=5
-
-booleans(""); //false like 0, -0
-booleans("hello"); //true like all numbers exept 0
+if (myNum==10){
+    document.write("myNum is equal than 10");
+} else{
+    document.write("myNum is not equal to 10")
+}
