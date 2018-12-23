3b5b74da441ee5f767e5650bfbc8cce6d456d781
Jan Grasewicz <j.grasewicz@gmail.com>
2018-12-23 15:36:40 +0100

    #38 removing elements from the DOM

--- test.js
+++ test.js
@@ -5,3 +5,9 @@ list.appendChild(newLi);
 newLi.appendChild(newA);
 newA.innerHTML="four";
 list.insertBefore(newLi, list.getElementsByTagName("li")[0])
+
+////
+
+var parent=document.getElementsByClassName("content")[1].getElementsByTagName("ul")[0];
+var child = parent.getElementsByTagName("li")[0];
+var removed =/* <--not needed, but good practise */parent.removeChild(child);
