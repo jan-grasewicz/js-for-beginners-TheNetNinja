9ff8971c1875167695ef06f093f45c4a32587ebd
Jan Grasewicz <j.grasewicz@gmail.com>
2018-12-23 12:20:44 +0100

    #30 constructor function

--- test.js
+++ test.js
@@ -1,26 +1,17 @@
-/*
-console.log(this);
-var myCar={
-    maxSpeed:70, 
-    driver:"Johnny", 
-    drive:function(speed, time){
-        console.log("distance traveled = "+speed*time)
-    },
-    test: function(){
-        console.log(this)
-    }
-};
-myCar.test();
-myCar.drive(50,3);
-*/
-var myCar={
-    maxSpeed:70, 
-    driver:"Johnny", 
-    drive:function(speed, time){
-        console.log("distance traveled = "+speed*time)
-    },
-    logDriver: function(){
-        console.log("driver name is "+ this.driver)
-    }
-};
-myCar.logDriver();
+
+//Construstor function
+var Car=function(maxSpeed,driver){
+    this.maxSpeed=maxSpeed;
+    this.driver=driver;
+    this.drive=function(speed, time){
+        console.log("distance traveled = "+speed*time)};
+    this.logDriver=function(){console.log("driver name is "+ this.driver)};
+}
+var myCar1=new Car(10,"exampe name1");
+var myCar2=new Car(20,"exampe name2");
+var myCar3=new Car(30,"exampe name3");
+var myCar4=new Car(40,"exampe name4");
+
+myCar1.drive(30,5);
+myCar3.logDriver();
+console.log(myCar4.maxSpeed);
