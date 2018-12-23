bd79d31287f80fefc7cb125fabb829fdebebb549
Jan Grasewicz <j.grasewicz@gmail.com>
2018-12-23 15:50:20 +0100

    #39 JS events

--- test.js
+++ test.js
@@ -1,13 +1,8 @@
-var newLi=document.createElement("li");
-var newA=document.createElement("a");
-var list = document.getElementsByClassName("content")[1].getElementsByTagName("ul")[0];
-list.appendChild(newLi);
-newLi.appendChild(newA);
-newA.innerHTML="four";
-list.insertBefore(newLi, list.getElementsByTagName("li")[0])
-
-////
-
-var parent=document.getElementsByClassName("content")[1].getElementsByTagName("ul")[0];
-var child = parent.getElementsByTagName("li")[0];
-var removed =/* <--not needed, but good practise */parent.removeChild(child);
+var title = document.getElementById("page-title");
+title.onclick = function(){
+    alert("you clicked me");
+};
+title.onmouseover=function(){
+    alert("you hoverd over me")
+};
+addEventListener() // just a mention
