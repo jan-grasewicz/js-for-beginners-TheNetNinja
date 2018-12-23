5d8b336324c05ce0053904a46b030a63131e5c2f
Jan Grasewicz <j.grasewicz@gmail.com>
2018-12-23 18:44:33 +0100

    #44 simple form validation

--- test.js
+++ test.js
@@ -1,7 +1,12 @@
 var myForm= document.forms.myForm;
-myForm.name.onfocus = function(){
-    myForm.name.style.border="4px solid pink";
-};
-myForm.name.onblur = function(){
-    myForm.name.style.border="";
-};
+var message=document.getElementById("message");
+
+myForm.onsubmit = function(){
+    if(myForm.name.value == ""){
+        message.innerHTML="please enter a name";
+        return false;
+    } else{
+        message.innerHTML = "";
+        return true;
+    }
+}
