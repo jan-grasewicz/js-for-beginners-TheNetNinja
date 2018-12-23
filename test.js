07b440730341ea420505ad8712f3be48faddf5dd
Jan Grasewicz <j.grasewicz@gmail.com>
2018-12-23 14:24:47 +0100

    #35 changing element attributes

--- test.js
+++ test.js
@@ -2,4 +2,17 @@ document.getElementsByClassName("content");
 var myContentDivs = document.getElementsByClassName("content");
 var myA = myContentDivs[1].getElementsByTagName("a");
 myA[2].innerHTML="change";
-document.getElementById("thelink").innerHTML="chchchchch";
+document.getElementById("thelink").textContent="chchchchch";
+
+
+var link = document.getElementById("linkid");
+console.log(link.getAttribute("class"));
+
+//link.setAttribute("class","pie");
+//console.log(link.getAttribute("class"));
+// =
+link.className = "pie";
+console.log(link.className);
+
+link.setAttribute("alt","new attribute"); //completely new attribute
+console.log(link.getAttribute("alt"));
