1642b755cfba596e8ff936414acd481fa7a5dced
Jan Grasewicz <j.grasewicz@gmail.com>
2018-12-23 18:01:15 +0100

    #42 timers

--- test.js
+++ test.js
@@ -1,21 +1,27 @@
-function setUpEvents(){
+/*
+var myMessage = document.getElementById("message");
 
-var content= document.getElementById("content");
-var button=document.getElementById("show-more");
-
-button.onclick=function(){
-    if(content.className=="open"){
-        //shrink the box
-        button.innerHTML="Show More";
-        content.className="";
-        
-    }else{
-        //expand the box
-        content.className="open";
-        button.innerHTML="Show Less";
-    }
-};
-};
-window.onload=function(){ //loads content of this function after loading the window
-setUpEvents(); 
+function showMessage(){
+    myMessage.className="show";
 }
+setTimeout(showMessage, 3000);
+*/
+
+var colorChanger = document.getElementById("color-changer");
+var colors = ["red","blue","green","pink"];
+var counter = 0;
+
+function changeColor(){
+    
+    if (counter >= colors.length){
+        counter=0;
+    }
+    colorChanger.style.background=colors[counter];
+    counter++;
+}
+var myTimer = setInterval(changeColor, 1000);
+
+colorChanger.onclick=function(){
+    clearInterval(myTimer);
+    colorChanger.innerHTML = "Timer stopped";
+};
